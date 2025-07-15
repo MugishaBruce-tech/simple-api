e5eb775a6bc389075634ba5fe8e1f200c4a5d9f7
MugishaBruce-tech <bruce.mugisha@mediabox.bi>
2025-07-15 13:55:05 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// A tiny Node.js server (save this as app.js)
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
